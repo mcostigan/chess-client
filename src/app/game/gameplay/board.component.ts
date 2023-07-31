4d4d36d13ce58d096817660fc2693088facdc5fc
Matt Costigan <costigan@uchicago.edu>
2023-07-31 16:01:59 -0500

    remove highlight after move and remove text based move selection

--- src/app/game/gameplay/board.component.ts
+++ src/app/game/gameplay/board.component.ts
@@ -7,14 +7,6 @@ import {IServerMove} from "../../../model/move";
 @Component({
   selector: 'board',
   template: `
-    <div>
-      <!--      TODO: better implementation-->
-      <input type="number" [(ngModel)]="fromRow"> <input type="number" [(ngModel)]="fromCol"> <input type="number"
-                                                                                                     [(ngModel)]="toRow">
-      <input type="number" [(ngModel)]="toCol">
-      <button (click)="move()">Move</button>
-    </div>
-
     <div class="board">
       <div class="rank" *ngFor="let rank of board.squares; let i = index">
         <app-square [position]="[i,j]" [square]="square"
@@ -42,12 +34,7 @@ import {IServerMove} from "../../../model/move";
 export class BoardComponent implements OnInit {
   @Input() board!: Board
 
-  private gameId: string
-
-  fromRow: number = -1
-  fromCol: number = -1
-  toRow: number = -1
-  toCol: number = -1
+  private readonly gameId: string
 
   constructor(private gameService: GameService, private activatedRoute: ActivatedRoute) {
     this.gameId = this.activatedRoute.snapshot.paramMap.get('gameId')!!
@@ -56,15 +43,9 @@ export class BoardComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  move() {
-    this.gameService.sendMove(this.gameId, {
-      from: {first: this.fromRow, second: this.fromCol},
-      to: {first: this.toRow, second: this.toCol}
-    })
-  }
-
   sendMove(move: IServerMove) {
     this.gameService.sendMove(this.gameId, move)
+    this.removeHighlight()
   }
 
   highlightMoves(moves: IServerMove[]) {
