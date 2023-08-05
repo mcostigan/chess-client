73e7ddc20aa5c5eee5d290330dd69a4c7f09eac8
Matt Costigan <costigan@uchicago.edu>
2023-08-05 16:07:09 -0500

    improve game creation ui

--- src/app/game/new-game.component.ts
+++ src/app/game/new-game.component.ts
@@ -4,11 +4,44 @@ import {GameService} from "./game.service";
 @Component({
   selector: 'app-game',
   template: `
-    <button (click)="createGame()">Create</button>
+    <div class="new-game">
+      <div class="center-horiz">
+        <h1>Create Game</h1>
+      </div>
+      <div class="center-horiz">
+        <input [(ngModel)]="minExperience" placeholder="Minimum Experience">
+      </div>
+      <div class="center-horiz">
+        <input [(ngModel)]="maxExperience" placeholder="Maximum Experience">
+      </div>
+      <div class="center-horiz">
+        <button (click)="createGame()">Create</button>
+      </div>
+
+    </div>
+
   `,
-  styles: []
+  styles: [
+    `
+      input, button {
+        width: 200px;
+      }
+
+      button {
+        border-radius: 50px;
+      }
+
+      .new-game {
+        display: flex;
+        flex-direction: column;
+        row-gap: 10px;
+        width: 100%;
+      }`
+  ]
 })
 export class NewGameComponent implements OnInit {
+  minExperience: string = ""
+  maxExperience: string = ""
 
   constructor(private gameService: GameService) {
   }
@@ -17,7 +50,7 @@ export class NewGameComponent implements OnInit {
   }
 
   createGame() {
-    this.gameService.createGame()
+    this.gameService.createGame(this.minExperience, this.maxExperience)
   }
 
 }
